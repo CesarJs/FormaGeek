0e01ec04bc94146038ca86f5a7192f9cf330c406
CesarJs <csr2314@gmail.com>
2019-07-07 14:01:36 -0300

    bf calculate

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,23 @@
 
 Route::get('/', function () {
 	$user = Auth::loginUsingId(1, true);
-	$user->wight =  $user->measure->last()->weight;
+	$user->weight =  $user->measure->last()->weight;
 	$user->height = $user->measure->last()->height;
-	
+	$user->neck = $user->measure->last()->neck;
+	$user->abdomen = $user->measure->last()->abdomen;
+	$user->waist = $user->measure->last()->waist;
+	$user->hip = $user->measure->last()->hip;
+
 	$user->imc = ($user->measure->last()->weight) / (($user->measure->last()->height*$user->measure->last()->height)/ 10000);
 
+	$user->bf = 86.010*log($user->abdomen - $user->neck) - 70.041 * log($user->weight);
 
-	
-	dd($user->imc);
+	/*$user->bf_feminino =163.205 * log($user->waist + $user->hip - $user->neck);  (97.684 * log($user->weight) -78.387);*/
+	$user->bf_feminino =163.203*log(45)-97.684*log(65) -87.387;
+
+
+
+	dd($user->bf_feminino);
     return view('welcome');
 });
 
